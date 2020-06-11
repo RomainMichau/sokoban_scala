3966e7a67dfaa7f65d09af6bd6ee24172a2e18e9
rmichau <r.michau@criteo.com>
2020-06-11 20:03:59 +0200

    add gui

--- src/main/scala/SokobanSolver.scala
+++ src/main/scala/SokobanSolver.scala
@@ -1,21 +1,12 @@
 import java.awt.BorderLayout
 
 import com.rmichau.soko.Console.ConsoleHelper
-import com.rmichau.soko.GUI.gui
+import com.rmichau.soko.GUI.SokoGui
 import com.rmichau.soko.Maze.Maze
 import javax.swing.{JFrame, JScrollPane, JTextArea}
 
 import scala.swing.Dimension
 
-object SokoGUI extends App{
-  val textArea = new JTextArea
-  textArea.setText("Hello, Swing world")
-  val scrollPane = new JScrollPane(textArea)
-
-  val frame = new JFrame("Hello, Swing")
-  frame.getContentPane.add(scrollPane, BorderLayout.CENTER)
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-  frame.setSize(new Dimension(600, 400))
-  frame.setLocationRelativeTo(null)
-  frame.setVisible(true)
+object SokobanSolver extends App{
+  val gui = new SokoGui
 }
--- src/main/scala/com/rmichau/soko/GUI/gui.scala
+++ src/main/scala/com/rmichau/soko/GUI/gui.scala
@@ -5,6 +5,15 @@ import java.awt.BorderLayout
 import javax.swing.{JFrame, JScrollPane, JTextArea}
 
 import scala.swing.Dimension
+class SokoGui{
 
-object gui extends App{
-}
+val textArea = new JTextArea
+textArea.setText("Hello, Swing world")
+val scrollPane = new JScrollPane(textArea)
+
+val frame = new JFrame("Hello, Swing")
+frame.getContentPane.add(scrollPane, BorderLayout.CENTER)
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+frame.setSize(new Dimension(600, 400))
+frame.setLocationRelativeTo(null)
+frame.setVisible(true)}
