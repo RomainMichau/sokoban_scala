d6234a4c7890d22b6f4244f2a4c0afc91d2f3dd1
rmichau <r.michau@criteo.com>
2020-06-27 18:30:04 +0200

    re-add test

--- src/test/scala/SokobanTest.scala
+++ src/test/scala/SokobanTest.scala
@@ -5,14 +5,15 @@ import com.rmichau.soko.Maze.Maze
 import com.rmichau.soko.util.Util
 import org.scalatest.FunSuite
 
-class SokobanTest extends FunSuite {/*
+class SokobanTest extends FunSuite {
  val levels: Array[File] = Util.recursiveListFiles(new File("test/levels")).filter(_.isFile)
  test("level file reader"){
-  assert(levels.toSeq.toString() == "WrappedArray(test/levels/test/test_1.dat, test/levels/test/test_4.dat, test/levels/test/test_2.dat, test/levels/easy/easy_1.dat, test/levels/easy/easy_2.dat)")
+  assert(levels.toSeq.toString() == "WrappedArray(test/levels/test/test_1.dat, test/levels/test/test_4.dat, test/levels/test/test_2.dat" +
+    ", test/levels/easy/easy_1.dat, test/levels/easy/easy_2.dat)")
  }
  val maze = new Maze(levels.filter(_.toURI.toString.contains("easy_1"))(0).toURI)
  test("test maze loading + static deadlock detection"){
   assert(maze.toString=="111111\n194111\n100111\n135091\n100291\n199111\n111111\n111111\n")
- }*/
+ }
 }
 
