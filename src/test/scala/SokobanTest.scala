0439a08fb60a8ca4e2874532dd69be2cc86c2067
rmichau <r.michau@criteo.com>
2020-06-29 20:06:28 +0200

    add m1 test

--- src/test/scala/SokobanTest.scala
+++ src/test/scala/SokobanTest.scala
@@ -10,19 +10,28 @@ import org.scalatest.FunSuite
 class SokobanTest extends FunSuite {
  val levels: Array[File] = Util.recursiveListFiles(new File("test/levels")).filter(_.isFile)
  test("level file reader"){
-  assert(levels.toSeq.toString() == "WrappedArray(test/levels/test/test_1.dat, test/levels/test/test_4.dat, test/levels/test/test_2.dat" +
-    ", test/levels/easy/easy_1.dat, test/levels/easy/easy_2.dat)")
+  assert(levels.toSeq.toString() == "WrappedArray(test/levels/medium/medium_3.dat, test/levels/medium/medium_2.dat, test/levels/medium/medium_1.dat," +
+    " test/levels/test/test_1.dat, test/levels/test/test_4.dat, test/levels/test/test_2.dat, test/levels/easy/easy_1.dat, test/levels/easy/easy_2.dat)")
  }
- val maze = new Maze(levels.filter(_.toURI.toString.contains("easy_1"))(0).toURI)
+ val mazee1 = new Maze(levels.filter(_.toURI.toString.contains("easy_1"))(0).toURI)
  test("test maze loading + static deadlock detection"){
-  assert(maze.toString=="111111\n194111\n100111\n135091\n100291\n199111\n111111\n111111\n")
+  assert(mazee1.toString=="111111\n194111\n100111\n135091\n100291\n199111\n111111\n111111\n")
  }
- val mazeSolver: MazeSolver = new MazeSolver(maze)
- val solvedMaze: BFS.BFSResult[PushBoxNode] = mazeSolver.solveMaze()
+ val mazeSolver: MazeSolver = new MazeSolver(mazee1)
+ val solvedMazeE1: BFS.BFSResult[PushBoxNode] = mazeSolver.solveMaze()
 
  test("easy1 should be solved"){
-  assert(solvedMaze.finalNode.isDefined)
-  assert(solvedMaze.finalNode.get.toDirs.size > 1)
+  assert(solvedMazeE1.finalNode.isDefined)
+  assert(solvedMazeE1.finalNode.get.toDirs.size > 1)
+ }
+
+ val mazem1 = new Maze(levels.filter(_.toURI.toString.contains("medium_1"))(0).toURI)
+ mazeSolver.setMaze(mazem1)
+ val solvedMazeM1: BFS.BFSResult[PushBoxNode] = mazeSolver.solveMaze()
+
+ test("medium 1 should be solved"){
+  assert(solvedMazeM1.finalNode.isDefined)
+  assert(solvedMazeM1.finalNode.get.toDirs.size > 1)
  }
 }
 
