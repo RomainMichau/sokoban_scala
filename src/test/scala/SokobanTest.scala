d11c59274ce3e03bcd944112cd332f1dbaea15cc
rmichau <r.michau@criteo.com>
2020-06-29 19:59:59 +0200

    add new test

--- src/test/scala/SokobanTest.scala
+++ src/test/scala/SokobanTest.scala
@@ -2,6 +2,8 @@ import java.io.File
 
 import com.rmichau.soko.GUI.LevelPicker
 import com.rmichau.soko.Maze.Maze
+import com.rmichau.soko.Solver.Node.PushBoxNode
+import com.rmichau.soko.Solver.{BFS, MazeSolver}
 import com.rmichau.soko.util.Util
 import org.scalatest.FunSuite
 
@@ -14,6 +16,13 @@ class SokobanTest extends FunSuite {
  val maze = new Maze(levels.filter(_.toURI.toString.contains("easy_1"))(0).toURI)
  test("test maze loading + static deadlock detection"){
   assert(maze.toString=="111111\n194111\n100111\n135091\n100291\n199111\n111111\n111111\n")
+ }
+ val mazeSolver: MazeSolver = new MazeSolver(maze)
+ val solvedMaze: BFS.BFSResult[PushBoxNode] = mazeSolver.solveMaze()
+
+ test("easy1 should be solved"){
+  assert(solvedMaze.finalNode.isDefined)
+  assert(solvedMaze.finalNode.get.toDirs.size > 1)
  }
 }
 
